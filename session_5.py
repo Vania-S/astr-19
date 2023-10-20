596ea3219432126eb0b13480419a7c3c86db124d
Vania-S <146883278+Vania-S@users.noreply.github.com>
2023-10-20 14:58:15 -0700

    Update session_5.py

--- session_5.py
+++ session_5.py
@@ -1,10 +1,17 @@
 import numpy as np #for working with numbers
 from astropy.table import Table #for creating the table class
 
-pi = np.pi  # pi doesn't mean anything unless yo apply numpy to it. they it has the value of pi
-x = np.linspace(0,pi*2,1000) # x creates the line space of range 0 to 2 pi with 1000 values between
-y = np.sin(x) # the function that each x is plugged into
+def graph():
 
-data = Table([x,y], names=['x','y']) #creates the table and structure and names each columN
+  pi = np.pi  # pi doesn't mean anything unless yo apply numpy to it. they it has the value of pi
+  x = np.linspace(0,pi*2,1000) # x creates the line space of range 0 to 2 pi with 1000 values between
+  y = np.sin(x) # the function that each x is plugged into
+  data = Table([x,y], names=['x','y']) #creates the table and structure and names each columN
 
-print(data)  #prints the function
+  print(data)  #prints the function
+
+def main():
+  graph()
+
+if __name__ == "__main__":
+  main()
